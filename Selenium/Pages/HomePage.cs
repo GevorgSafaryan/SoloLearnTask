257c3967bf34f46dd914b34998ee65fefe4bae07
GevorgSafaryan <gevorgsafarjan@gmail.com>
2019-05-19 16:17:04 +0400

    1

--- Selenium/Pages/HomePage.cs
+++ Selenium/Pages/HomePage.cs
@@ -1,11 +1,6 @@
 ﻿using OpenQA.Selenium;
 using Selenium.Engine;
 using SeleniumExtras.PageObjects;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Selenium.Pages
 {
